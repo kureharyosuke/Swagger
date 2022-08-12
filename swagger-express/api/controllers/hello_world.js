62d08c85af77c011cb80e606430a6ef1e87c5eba
kureharyosuke <ohhayo.com@gmail.com>
2022-08-12 19:49:02 +0900

    #2 install: swagger-tools & node -v 14 down

--- swagger-express/api/controllers/hello_world.js
+++ swagger-express/api/controllers/hello_world.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*
  'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
@@ -10,7 +10,7 @@
 
   It is a good idea to list the modules that your application depends on in the package.json in the project root
  */
-var util = require('util');
+var util = require("util");
 
 /*
  Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
@@ -25,7 +25,7 @@ var util = require('util');
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  hello: hello
+  hello: hello,
 };
 
 /*
@@ -36,8 +36,8 @@ module.exports = {
  */
 function hello(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+  var name = req.swagger.params.name.value || "stranger";
+  var hello = util.format("Hello, %s!", name);
 
   // this sends back a JSON response which is a single string
   res.json(hello);
--- swagger-express/app.js
+++ swagger-express/app.js
@@ -1,23 +1,30 @@
-'use strict';
+"use strict";
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var SwaggerExpress = require("swagger-express-mw");
+var SwaggerUi = require("swagger-tools/middleware/swagger-ui");
+var app = require("express")();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+  if (err) {
+    throw err;
+  }
 
   // install middleware
+  swaggerExpress.runner.swagger.host = "127.0.0.1:10010";
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  if (swaggerExpress.runner.swagger.paths["/hello"]) {
+    console.log(
+      "try this:\ncurl http://127.0.0.1:" + port + "/hello?name=Scott"
+    );
   }
 });
